14bd7431eb89b0f677cf59b5e06af2dd4db256c1
PowaPuffGirl <a.stollberger@web.de>
2018-04-09 00:09:58 +0200

    test push

--- push.sh
+++ push.sh
@@ -4,6 +4,9 @@ then
 	echo no commit message
 else
 	cp -R ~/.config/i3 i3 
+	cp -R ~/.config/i3blocks i3blocks
+	cp -R ~/.config/terminator terminator
+	cp ~.Xresources .Xresources 
 	git add .
 	git commit -m "$1"
 	git push origin master
