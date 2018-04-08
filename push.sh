43ea70df749cab917bc71c997be178698b4806dc
PowaPuffGirl <a.stollberger@web.de>
2018-04-09 00:11:34 +0200

    test push

--- push.sh
+++ push.sh
@@ -6,7 +6,7 @@ else
 	cp -R ~/.config/i3 i3 
 	cp -R ~/.config/i3blocks i3blocks
 	cp -R ~/.config/terminator terminator
-	cp ~.Xresources .Xresources 
+	cp ~/.Xresources .Xresources 
 	git add .
 	git commit -m "$1"
 	git push origin master
