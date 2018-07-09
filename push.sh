5ba4a6d9015f07f49c9739d2b7c70d06a53542db
PowaPuffGirl <a.stollberger@web.de>
2018-07-09 18:00:03 +0200

    no Message

--- push.sh
+++ push.sh
@@ -9,16 +9,16 @@ fi
 
 
 
-cd /home/buttercup/git/I3Backup
+cd ~/git/I3Backup
 rm -R i3
 rm -R i3blocks
 rm -R terminator
 rm .Xresources
 
-cp -R /home/buttercup/.config/i3 i3 
-cp -R /home/buttercup/.config/i3blocks i3blocks
-cp -R /home/buttercup/.config/terminator terminator
-cp /home/buttercup/.Xresources .Xresources
+cp -R ~/.config/i3 i3 
+cp -R ~/.config/i3blocks i3blocks
+cp -R ~/.config/terminator terminator
+cp ~/.Xresources .Xresources
 
 git add .
 git commit -m "$message"
