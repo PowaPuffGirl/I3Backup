58653e9025a7929b6695f255cfeac52c3fe39668
buttercup <a.stollberger@web.de>
2018-11-10 18:09:09 +0100

    no Message

--- push.sh
+++ push.sh
@@ -15,12 +15,14 @@ rm -R i3blocks
 rm -R terminator
 rm .Xresources
 rm -R lightdm
+rm .zshrc
 
 cp -R ~/.config/i3 i3 
 cp -R ~/.config/i3blocks i3blocks
 cp -R ~/.config/terminator terminator
 cp ~/.Xresources .Xresources
-cp /etc/lightdm/
+cp -R /etc/lightdm/
+cp ~/.zshrc .zshrc
 
 git add .
 git commit -m "$message"
