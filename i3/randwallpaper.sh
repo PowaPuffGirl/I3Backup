0600eac8b86ca89d818762900b9f653caf1927ba
buttercup <a.stollberger@web.de>
2019-01-09 13:48:47 +0100

    changed matlab script to change screen resolution

--- i3/randwallpaper.sh
+++ i3/randwallpaper.sh
@@ -3,5 +3,4 @@ cd /home/buttercup/Wallpapers
 imageAmount=$( ls -la |wc -l)
 imageAmount=$(( $imageAmount - 3 ))
 rand=$(( ( RANDOM % $imageAmount ) + 1 ))
-i3lock -t -i ./Wallpaper$rand.png
-
+feh --bg-scale ./Wallpaper$rand.png
