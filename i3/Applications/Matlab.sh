6cd145973766684fd863bee192313d8cd3d78cce
buttercup <a.stollberger@web.de>
2019-04-24 15:14:20 +0200

    added chromium to Applist

--- i3/Applications/Matlab.sh
+++ i3/Applications/Matlab.sh
@@ -2,6 +2,6 @@
 cd home/buttercup/git/Bachelorarbeit/MatlabScripts
 $( xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --panning 1920x1080+0+0 --scale 1x1  --output HDMI2 --off --output HDMI1 --off --output DP2 --off )
 $( xrandr --output eDP1 --panning 1920x1080+0+0 )
-~/Matlab/bin/matlab -desktop
+#~/Matlab/bin/matlab -desktop
 
-sh ~/.config/i3/ScreenCommands/Reset.sh
+#sh ~/.config/i3/ScreenCommands/Reset.sh
